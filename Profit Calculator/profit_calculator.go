f7530d614f7cd2e29bc38b0f3acd1ad4dcaebf1e
Omar Khalid <137959170+omarovici@users.noreply.github.com>
2026-04-03 16:15:12 +0200

    Profit Calculator

---
+++ Profit Calculator/profit_calculator.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func main() {
+	var revenue, expenses, taxRate float64
+	fmt.Print("Revenue: ")
+	fmt.Scan(&revenue)
+	fmt.Print("Expenses: ")
+	fmt.Scan(&expenses)
+	fmt.Print("Tax Rate: ")
+	fmt.Scan(&taxRate)
+
+	earningsBeforeTax := revenue - expenses
+	profit := earningsBeforeTax * (1 - taxRate/100)
+	ratio := earningsBeforeTax / profit
+
+	fmt.Println("Earnings before tax: ", earningsBeforeTax)
+	fmt.Println("Profit: ", profit)
+	fmt.Println("Ratio: ", ratio)
+}
