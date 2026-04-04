7e96effb31fdc3f4416ba78a5a17b288e0f07f8a
Omar Khalid <137959170+omarovici@users.noreply.github.com>
2026-04-04 16:17:07 +0200

    Functions: Return Multiple Values

--- Investment Calculator/investment_calculator.go
+++ Investment Calculator/investment_calculator.go
@@ -5,8 +5,9 @@ import (
 	"math"
 )
 
+const inflationRate = 2.5
+
 func main() {
-	const inflationRate = 2.5
 	var investmentAmount float64
 	var years float64
 	var expectedReturnRate float64
@@ -23,8 +24,9 @@ func main() {
 	outputText("Years: ")
 	fmt.Scan(&years)
 
-	futureValue := investmentAmount * math.Pow(1+expectedReturnRate/100, years)
-	futureRealValue := futureValue / math.Pow(1+inflationRate/100, years)
+	futureValue, futureRealValue := calculateFutureValues(investmentAmount, expectedReturnRate, years)
+	//futureValue := investmentAmount * math.Pow(1+expectedReturnRate/100, years)
+	//futureRealValue := futureValue / math.Pow(1+inflationRate/100, years)
 
 	//formatedFV := fmt.Sprintf("Future Value %.1f\n", futureValue)
 	//formatedRFV := fmt.Sprintf("Future Real Value (adjusted for inflation): %.1f\n", futureRealValue)
@@ -36,9 +38,15 @@ func main() {
 	//fmt.Printf("Future Value: %.1f\nFuture Real Value (adjusted for inflation): %.1f\n ", futureRealValue, futureRealValue)
 	//fmt.Print(formatedFV, formatedRFV)
 	fmt.Printf(`Future Value: %.1f
-	Future Real Value (adjusted for inflation): %.1f\n`, futureRealValue, futureRealValue)
+	Future Real Value (adjusted for inflation): %.1f\n`, futureValue, futureRealValue)
 }
 
 func outputText(text string) {
 	fmt.Print(text)
+}
+
+func calculateFutureValues(investmentAmount, expectedReturnRate, years float64) (float64, float64) {
+	fv := investmentAmount * math.Pow(1+expectedReturnRate/100, years)
+	rfv := fv / math.Pow(1+inflationRate/100, years)
+	return fv, rfv
 }
