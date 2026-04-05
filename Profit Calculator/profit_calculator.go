76dbe8ef6b7ced045ffb0df469eee0ec75ab45e6
Omar Khalid <137959170+omarovici@users.noreply.github.com>
2026-04-05 10:28:23 +0200

    Profit Calculator Enhancements

--- Profit Calculator/profit_calculator.go
+++ Profit Calculator/profit_calculator.go
@@ -3,19 +3,36 @@ package main
 import "fmt"
 
 func main() {
-	var revenue, expenses, taxRate float64
-	fmt.Print("Revenue: ")
-	fmt.Scan(&revenue)
-	fmt.Print("Expenses: ")
-	fmt.Scan(&expenses)
-	fmt.Print("Tax Rate: ")
-	fmt.Scan(&taxRate)
+	//var revenue, expenses, taxRate float64
+	//fmt.Print("Revenue: ")
+	//fmt.Scan(&revenue)
+	//fmt.Print("Expenses: ")
+	//fmt.Scan(&expenses)
+	//fmt.Print("Tax Rate: ")
+	//fmt.Scan(&taxRate)
+	revenue := textInputUser("Revenue: ")
+	expenses := textInputUser("Expenses: ")
+	taxRate := textInputUser("Taxes: ")
 
-	earningsBeforeTax := revenue - expenses
-	profit := earningsBeforeTax * (1 - taxRate/100)
-	ratio := earningsBeforeTax / profit
+	//earningsBeforeTax := revenue - expenses
+	//profit := earningsBeforeTax * (1 - taxRate/100)
+	//ratio := earningsBeforeTax / profit
+	earningsBeforeTax, profit, ratio := profitCalculatorValue(revenue, expenses, taxRate)
 
-	fmt.Println("Earnings before tax: ", earningsBeforeTax)
-	fmt.Println("Profit: ", profit)
-	fmt.Println("Ratio: ", ratio)
+	fmt.Printf("Earnings before tax: %.1f\n", earningsBeforeTax)
+	fmt.Printf("Profit: %.1f\n", profit)
+	fmt.Printf("Ratio: %.1f\n", ratio)
+}
+
+func profitCalculatorValue(revenue, expenses, taxRate float64) (ebt, profit, ratio float64) {
+	ebt = revenue - expenses
+	profit = ebt * (1 - taxRate/100)
+	ratio = ebt / profit
+	return
+}
+
+func textInputUser(text string) (value float64) {
+	fmt.Print(text)
+	fmt.Scan(&value)
+	return
 }
